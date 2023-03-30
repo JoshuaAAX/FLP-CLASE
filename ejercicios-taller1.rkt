f3bcfff4092a71e48b9296555ef9825e2dab78fc
Damian Espinosa <111003089+AsherAST@users.noreply.github.com>
2023-03-30 14:43:44 -0500

    add ejercicio 1

--- ejercicios-taller1.rkt
+++ ejercicios-taller1.rkt
@@ -1,4 +1,50 @@
+#lang eopl
 
+;;Joshua Chicame - 2074121
+;;Damian Espinosa - 2028180
+;; Taller 1 FLP
+
+;; Ejercicio 1 /Append:
+;; L1 L2 -> L
+;; Proposito: Retorna una lista con todos los elementos de las listas de argumento dadas.
+;; <lista> := ()
+;;         := (<elemento> <lista>)
+
+
+(define Append
+  (lambda (l1 l2)
+     (if(null? l1)
+        l2
+         (cons (car l1) (Append (cdr l1) l2)))))
+
+;; Ejercicio 1 / aux-invert:
+;; L -> L
+;; Proposito: invierte los elementos de una lista
+;; <lista> := ()
+;;         := (<elemento> <lista>)
+
+
+;; Ejercicio 1 / invert:
+;; L -> L
+;; Proposito: retorna la lista con sus pares ordenados invertidos  si cumplen con el predicado
+;; <lista-par> := ()
+;;             := ((<elemento> <elemento>) <lista-par>)
+(define invert
+ (lambda (L P)
+    (if (null? L)
+        L
+        (if (and (P (caar L)) (P (cadar L)) )
+            (cons (Append (cdar L) (cons (caar L) empty) ) (invert (cdr L) P))
+            (invert (cdr L) P)
+        )
+     )
+  )
+)
+
+;; Pruebas
+ 
+(invert '((3 2) (4 2) (1 5) (2 8)) even?)
+(invert '((6 9) (10 90) (82 7) ) odd? )
 
 ;; Ejercicio 2 / down:
 ;; L -> L
