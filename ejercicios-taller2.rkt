b2056cc58c4941ff27d052f82fd4e6445d898743
JoshuaAAX <81261464+JoshuaAAX@users.noreply.github.com>
2023-04-17 22:22:01 -0500

    add file2

---
+++ ejercicios-taller2.rkt
@@ -0,0 +1,63 @@
+;; <expresion> := <clausula> | <clausla> AND <clausula>
+;; <clausula>:= <varible> | <variable> OR <variable>
+;; <variable> := <digito> | NOT <digito>
+;; <digito> := 1|2|...|n
+                  
+
+#lang eopl
+
+(define digito
+   (lambda (n)
+      (list 'digito n)
+   )
+)
+
+(define digito?
+   (lambda (digito)
+      (if (number? (cadr digito))
+          #t
+          #f
+      )
+   )
+)
+
+(define (negation-of-digito? exp)
+  (and (eq? (car exp) 'NOT)
+     #t
+  )
+)
+
+(define variable?
+  (lambda (variable)
+    (or  (digito? variable)
+        (negation-of-digito?  variable)
+    )
+  )
+)
+
+
+(define clausula
+    (lambda (variable1  variable2)
+      (list variable1'or variable2)
+    )
+)
+
+
+(define expresion
+    (lambda (clausula1  clausula2)
+      (list  clausula1 'and clausula2)
+    )
+)
+
+
+(define expresion? 
+  (lambda (exp)
+     (or (number? exp)
+       (and (pair? exp)
+           (eq? (cadr exp) 'AND)
+           (number? (car exp))
+           (expresion? (cddr exp))
+        )
+     )
+  )
+)
