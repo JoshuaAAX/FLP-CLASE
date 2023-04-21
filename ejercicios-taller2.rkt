a36785970240b81d56a91fb4999da7f5478319a4
JoshuaAAX <81261464+JoshuaAAX@users.noreply.github.com>
2023-04-21 15:38:18 -0500

    add datatypes

--- ejercicios-taller2.rkt
+++ ejercicios-taller2.rkt
@@ -72,3 +72,38 @@
 ; Ejemplo de uso:
 ; (combi-booleano 2) => ((#t #t) (#t #f) (#f #t) (#f #f))
 ; (combi-booleano 3) => ((#t #t #t) (#t #t #f) (#t #f #t) (#t #f #f) (#f #t #t) (#f #t #f) (#f #f #t) (#f #f #f))
+
+
+;; <expresion> := <clausula> | (<clausula>) AND (<clausula>)
+;; <clausula>:= <varible> | <variable> OR <variable>
+;; <variable> := <digito> | NOT <digito>
+;; <digito> := 1|2|...|n
+                  
+
+#lang eopl
+
+(define-datatype bintree bintree?
+  (empty-b-tree)
+  (b-tree (num number?) (lson bintree?) (rson bintree?))
+)
+
+
+(define-datatype expresion expresion?
+   (una_clausula (clausula clausula?))
+   (dos_clausulas  (clausula1 clausula?) (clausula2 clausula?))
+)
+
+(define-datatype clausula clausula?
+   (una_variable (variable variable?) )
+   (dos_variables (variable1 variable?)  (variable2 variable?))
+)
+
+(define-datatype variable variable?
+   (digito_normal  (digito digito?) )
+   (digito_negado  (digito digito?) )
+)
+
+(define-datatype digito digito?
+   (numero (num number?))
+)
+
