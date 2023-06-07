ddbed18afbf9f7feb6dfe118ec486182c8e78d97
LuisaCardenasL <luisa072697@gmail.com>
2023-06-06 22:38:37 -0500

    gramatik finished

--- Mini-Py.rkt
+++ Mini-Py.rkt
@@ -86,3 +86,33 @@
 ;;                      <super-call-exp(method-name rands)>
 ;;<clausula-or>     ::= (<numero>)+("or")
 ;;                      <clausula-or-exp (n lsn)>
+;;<prim-un>         ::= solveFNC | lenght
+;;                  ::= add1 | sub1 | add1_16 | sub1_16
+;;                  ::= list? | head | tail
+;;<prim-bin>        ::= + | - | * | % | / | +_16 | -_16 | *_16
+;;                  ::= create-list | append | create-vec | create-reg
+;;                  ::= ref-vec | set-reg | concat
+;;<lista>           ::= empty
+;;                      <empty-list>
+;;                  ::= [{<expresion>}*(;)]
+;;                      <lista1 (lexps)>
+;;<vectorB>          ::= vector[{<expresion>}*(;)]
+;;                      <vector1 (lexps)>
+;;<registro>        ::= {{<identificador> = <expresion>}+(;)}
+;;                      <registro1 (id exp lids lexps)>
+;;<expr-bool>       ::= <pred-prim> (<expresion> , <expresion>)
+;;                      <comparacion (pprim exp1 exp2)>
+;;                  ::= <oper-bin-bool> (<expr-bool> , <expr-bool>)
+;;                      <conjuncion (obbool expb1 expb1)>
+;;                  ::= <bool>
+;;                      <vlr-bool (bool)>
+;;                  ::= <oper-un-bool> (<expr-bool>)
+;;                      <op-comp (oubool expb)>
+;;<to-odownto>      ::= to
+;;                      <to>
+;;                  ::= downto
+;;                      <downto>
+;;<pred-prim>       ::= <|>|<=|>=|==|!=
+;;<oper-bin-bool>   ::= and|or
+;;<oper-un-bool>    ::= not
+;;<bool>            ::= true | false
